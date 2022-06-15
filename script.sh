faab0aa72ad884c949530bc6b531bede0f50aa8f
voytus <voytus@protonmail.com>
2022-06-15 10:47:30 +0100

    WTM: added script again

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
- echo "node_modules/" > .gitignore && npm init && npx -create-react-app
+ echo "node_modules/" > .gitignore && npm init && npx -create-react-app name-of-new-app && cd name-of-new-app && npm start
+ 
