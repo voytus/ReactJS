89e154380f62a80426428d87b416722bc61b1ded
voytus <voytus@protonmail.com>
2022-06-15 15:28:43 +0100

    WTM: added example

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import './App.css'; 
-import First from './Components/First';
-import Second from './Components/Second';
-import Third from './Components/Third';
-import Title from './Components/Title';
+import './App.css';
+const SomeApp = () => {
 
+  const address = {
+      forename: "Nick",
+      surname: "Johnson",
+      line1: "QA Consulting",
+      line2: "Anchorage 1",
+      city: "Manchester",
+      zip: "M50 3YL"
+  }
 
-function App() {
-    return (
-    <>
-      <Title />
-      <First />
-      <Second />
-      <Third />
-    </>
-    );
+  return (
+      <div>
+          <h1>{address.forename}</h1>
+          <h1>{address.surname}</h1>
+          <h1>{address.line1}</h1>
+          <h1>{address.line2}</h1>
+          <h1>{address.city}</h1>
+          <h1>{address.zip}</h1>
+      </div>
+  );
 }
-export default App;
+export default SomeApp;
+
+
+
+
+
+// import First from './Components/First';
+// import Second from './Components/Second';
+// import Third from './Components/Third';
+// import Title from './Components/Title';
+
+
+// function App() {
+//     return (
+//     <>
+//       <Title />
+//       <First />
+//       <Second />
+//       <Third />
+//     </>
+//     );
+// }
+// export default App;
 
 
 // import logo from './logo.svg';
