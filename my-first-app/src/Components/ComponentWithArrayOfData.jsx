445757772a989e9ef9935c481065a9e83a568a3f
voytus <voytus@protonmail.com>
2022-06-15 16:22:55 +0100

    WTM: added ComponentWithArrayOfData.jsx fixed

--- my-first-app/src/Components/ComponentWithArrayOfData.jsx
+++ my-first-app/src/Components/ComponentWithArrayOfData.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
-const ComponentWithArrayOfData = () => {
+function ComponentWithArrayOfData() {
 
     const arrayOfListItems = [
-    'Wash the dishes',
-    'Walk the dog',
-    'Clean the windows',
-    'Mow the lawn'
-   ];
-  const listItems = arrayOfListItems.map(item => <li>(item)</li>);
-   return (
-        <ol>
-               {listItems}
-        </ol>
-   );
+        'Wash the dishes',
+        'Walk the dog',
+        'Clean the windows',
+        'Mow the lawn'
+    ];
+
+    //   const listItems = arrayOfListItems.map(item => <li>(item)</li>);
+    const listItems = arrayOfListItems.map(
+        (item, index) => <li key={index}>{item}</li>
+            );
+
+
+
+
+            return (
+            <ol>
+                {listItems}
+            </ol>
+            );
+       
 
 };
 
