d8c6c891c3b3e9083e962b077372139573ead03c
voytus <voytus@protonmail.com>
2022-06-16 11:25:28 +0100

    WTM: added ComponentConditionalTernaryRender.jsx

---
+++ my-first-app/src/Components/ComponentConditionalTernaryRender.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ComponentConditionalTernaryRender = () => {
+
+    const renderMe = true;
+    const dontRenderMe = false;
+
+    return (
+        <>
+        {
+            renderMe ?
+            <><p> It will be displayed if renderMe is true</p>
+            <p>It will be displayed if renderMe is false</p></>
+        }
+        </>
+    );
+
+};
+
+export default ComponentConditionalTernaryRender;
